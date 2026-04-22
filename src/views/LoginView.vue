f74c892878331d0cdb3797606acc5bda4a362fea
Руслан Эмиров <ruslanemirov@MacBook-Air.local>
2026-04-23 00:46:15 +0300

    Глава 3: верстка страницы логина

---
+++ src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" lg="6">
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Email"
+                type="email"
+                variant="outlined"
+                class="mb-3"
+              >
+              </v-text-field>
+              <v-text-field
+                label="Password"
+                type="password"
+                variant="outlined"
+              >
+              </v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'LoginView'
+}
+</script>
