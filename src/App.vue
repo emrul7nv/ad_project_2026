74c6c121dc07f543fb549f53988330038ee7fae0
Руслан Эмиров <ruslanemirov@MacBook-Air.local>
2026-04-23 00:26:11 +0300

    Глава 2: пункты 2.1, 2.2, 2.3 - layout, drawer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer">
+    </v-navigation-drawer>
+
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn>
+          <v-icon start>🍰</v-icon>
+          Link One
+        </v-btn>
+        <v-btn>Link Two</v-btn>
+        <v-btn>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
