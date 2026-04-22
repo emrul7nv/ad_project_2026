37d227a2c80e3fb79456eaa392a1b6c165437ec3
Руслан Эмиров <ruslanemirov@MacBook-Air.local>
2026-04-23 00:52:15 +0300

    Глава 3: форма логина с валидацией

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,25 +7,37 @@
             <v-toolbar-title>Login</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-            <v-form>
+            <v-form v-model="valid" ref="form" lazy-validation>
               <v-text-field
                 label="Email"
                 type="email"
                 variant="outlined"
                 class="mb-3"
+                v-model="email"
+                :rules="emailRules"
+                required
               >
               </v-text-field>
               <v-text-field
                 label="Password"
                 type="password"
                 variant="outlined"
+                v-model="password"
+                :rules="passwordRules"
+                required
               >
               </v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary">Login</v-btn>
+            <v-btn 
+              color="primary" 
+              @click="onSubmit"
+              :disabled="!valid"
+            >
+              Login
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -35,6 +47,32 @@
 
 <script>
 export default {
-  name: 'LoginView'
+  name: 'LoginView',
+  data() {
+    return {
+      email: "",
+      password: "",
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be at least 6 characters'
+      ]
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log('Login user:', user)
+      }
+    }
+  }
 }
 </script>
